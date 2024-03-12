1eab72c36cadc2c585aaf5b6f50d50b7a1b43453
IngrithR14 <nenita.yr34@gmail.com>
2024-03-12 18:53:42 -0500

    cambio

--- data/resolvers.js
+++ data/resolvers.js
@@ -1,5 +1,27 @@
+const findAllBands = () => {
+    fetch("https://taller-api.vercel.app/band", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify({
+          query,
+        }),
+      })
+        .then((respuesta) => respuesta.json())
+        .then((result)=>{
+            result.data
+        })
+    };   
 const resolvers = {
-
+    Query: {
+        getAllBands: async () => {
+            const band = await findAllBands()
+            return band;
+        },
+        
+    },
 }
 
 module.exports = { resolvers }
