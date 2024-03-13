6c96f3d9bc1b244fcfaae4cdcf990f391bc85041
Marc064 <95501065+Marc064@users.noreply.github.com>
2024-03-12 19:30:17 -0500

    Get band id

--- data/resolvers.js
+++ data/resolvers.js
@@ -17,12 +17,38 @@ const findAllBands = () => {
         });
 };
 
+const findBandId = (id) => {
+    return fetch(`https://taller-api.vercel.app/band/id/${id}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        }
+    })
+        .then((respuesta) => respuesta.json())
+        .then((result) => {
+            console.log(result.data);
+            return result.data[0];
+        })
+        .catch((error) => {
+            console.error("Error fetching bands:", error);
+            throw error;
+        });
+};
+
 const resolvers = {
     Query: {
         getAllBands: async () => {
             const band = await findAllBands()
             return band;
         },
+
+        getBandByID: async (_, args) => {
+            const id = args.id
+            const band = await findBandId(id)
+            console.log(band.name);
+            return band
+        }
 
     },
 }
